3301d22eccba5cb827c5716b5d6d38537b95a4f7
David Blaa <dblaa@bgc-jena.mpg.de>
2018-12-04 15:53:29 +0100

    first draft of a bexis2module template

---
+++ UI/Helper/TEMPLATESeedDataGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BExIS.Modules.TEMPLATE.UI.Helpers
+{
+    public class TEMPLATESeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
