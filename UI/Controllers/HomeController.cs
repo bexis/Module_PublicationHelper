f5a27f4077274c462277175feac1aacb6e842861
David Blaa <dblaa@bgc-jena.mpg.de>
2018-12-11 15:18:37 +0100

    example libaries for entities, mapping and services added

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Vaiona.Web.Mvc.Models;
+﻿using System.Web.Mvc;
 using Vaiona.Web.Extensions;
+using Vaiona.Web.Mvc.Models;
 
 namespace BExIS.Modules.TEMPLATE.UI.Controllers
 {
@@ -16,6 +12,12 @@ namespace BExIS.Modules.TEMPLATE.UI.Controllers
             ViewBag.Title = PresentationModel.GetViewTitleForTenant("TEMPLATE", this.Session.GetTenant());
             return View();
 
+        }
+
+        public ActionResult Index2()
+        {
+            ViewBag.Title = PresentationModel.GetViewTitleForTenant("TEMPLATE", this.Session.GetTenant());
+            return View();
         }
     }
 }
