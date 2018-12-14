fa49f6775f1449b0d1f581305792be381689e3d3
Sven Thiel <m6thsv2@googlemail.com>
2018-12-14 11:51:56 +0100

    modify renaming script and minor changes in readme

--- ModuleTemplate_Renaming.ps1
+++ ModuleTemplate_Renaming.ps1
@@ -2,10 +2,13 @@ $base_dir = Read-Host -Prompt 'Root Directory'
 $search = Read-Host -Prompt 'Replace'
 $replace_with = Read-Host -Prompt 'With'
 
-Get-ChildItem $base_dir -Recurse -Include "*.*" |
-    ForEach-Object { (Get-Content $_.FullName) | 
-    ForEach-Object {$_ -replace $search, $replace_with} | 
-    Set-Content $_.FullName }
+Get-ChildItem -Recurse -Include *$search* -Exclude *.Entities,*.NH,*.Services,ModuleTemplate_Renaming* |
+	Rename-Item -NewName { $_.Name -replace $search, $replace_with }
 
-Get-ChildItem $base_dir -Recurse |
-    Rename-Item -NewName {$_.name -replace $search, $replace_with}
+Get-ChildItem -Recurse -Include *$search* -Exclude ModuleTemplate_Renaming* |
+	Rename-Item -NewName { $_.Name -replace $search, $replace_with }
+	
+Get-ChildItem -Recurse -Include *.* -Exclude ModuleTemplate_Renaming*,*.Entities,*.NH,*.Services |
+    ForEach-Object { (Get-Content $_ | 
+    ForEach-Object { $_ -replace "$search", "$replace_with" }) | 
+    Set-Content $_ }
