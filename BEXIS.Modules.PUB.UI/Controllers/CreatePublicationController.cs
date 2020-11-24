5f42448b566ffe5e18c98a029f08472098c24b41
Petzold <eleonora.petzold@uni-jena.de>
2020-11-24 15:27:18 +0100

    provide more then one metadata structures for publications #25

--- BEXIS.Modules.PUB.UI/Controllers/CreatePublicationController.cs
+++ BEXIS.Modules.PUB.UI/Controllers/CreatePublicationController.cs
@@ -10,6 +10,7 @@ using BExIS.Dlm.Services.Data;
 using BExIS.Dlm.Services.DataStructure;
 using BExIS.Dlm.Services.MetadataStructure;
 using BExIS.Modules.Dcm.UI.Controllers;
+using BExIS.Modules.PUB.UI.Models;
 using BExIS.Security.Entities.Authorization;
 using BExIS.Security.Entities.Subjects;
 using BExIS.Security.Services.Authorization;
@@ -38,52 +39,131 @@ namespace BExIS.Modules.PUB.UI.Controllers
         // GET: CreatePublication
         public ActionResult Index()
         {
-           
-            using (var metadataStructureManager = new MetadataStructureManager())
-            using (var entityManager = new EntityManager())
-            using (var researchPlanManager = new ResearchPlanManager())
+            string mulitbleMetadataStructure = Helper.Settings.get("MultibleMetadataStructures").ToString();
+
+            if (mulitbleMetadataStructure == "true")
             {
-                //get metadatastruture
-                var metadataStructureName = Helper.Settings.get("MetadataStructureName");
+                SetupModel model = new SetupModel();
 
-                var md = metadataStructureManager.Repo.Get(a => a.Name == metadataStructureName.ToString()).FirstOrDefault();
-
-                //get entitytype
-                var entityType = entityManager.FindByName("Publication");
-
-                if (md != null)
+                using (MetadataStructureManager metadataStructureManager = new MetadataStructureManager())
                 {
-                    CreateTaskmanager taskManager = new CreateTaskmanager();
-                    taskManager.AddToBus(CreateTaskmanager.METADATASTRUCTURE_ID, md.Id);
-                  
-                    taskManager.AddToBus(CreateTaskmanager.SAVE_WITH_ERRORS, false);
-                    taskManager.AddToBus(CreateTaskmanager.NO_IMPORT_ACTION, true);
+                    IEnumerable<MetadataStructure> metadataStructureList = metadataStructureManager.Repo.Get();
 
-                   // get existing researchPlan
-                   string researchPlanName = "Research plan";
-                   ResearchPlan researchPlan = researchPlanManager.Repo.Get(r => researchPlanName.Equals(r.Title)).FirstOrDefault();
-                   taskManager.AddToBus(CreateTaskmanager.RESEARCHPLAN_ID, researchPlan.Id);
+                    foreach (MetadataStructure metadataStructure in metadataStructureList)
+                    {
+                        if (xmlDatasetHelper.IsActive(metadataStructure.Id) &&
+                            HasEntityTypeName(metadataStructure, "Publication"))
+                        {
+                            string title = metadataStructure.Name;
 
-                    //create unstructured datastructure
-                    DataStructure dataStructure = CreateDataStructure("Publication-File") as UnStructuredDataStructure;
-                    taskManager.AddToBus(CreateTaskmanager.DATASTRUCTURE_ID, dataStructure.Id);
+                            model.MetadataStructureList.Add(new ViewSelectItem(metadataStructure.Id, title));
+                        }
+                    }
 
-                    HttpContext.Session["CreateDatasetTaskmanager"] = taskManager;
-                    setAdditionalFunctions();
-
-                    var view = this.Render("DCM", "Form", "StartMetadataEditor", new RouteValueDictionary()
-                    { });
-
-                    return Content(view.ToHtmlString(), "text/html");
+                    model.MetadataStructureList.OrderBy(p => p.Title);
                 }
 
-                else
+               return View("ChooseMetadataStructure", model);
+            }
+            else
+            {
+                using (var metadataStructureManager = new MetadataStructureManager())
+                using (var entityManager = new EntityManager())
+                using (var researchPlanManager = new ResearchPlanManager())
                 {
-                    return Content("Metadata structure not exsits");
+                    //get metadatastruture
+                    var metadataStructureName = Helper.Settings.get("DefaultMetadataStructure");
+
+                    var md = metadataStructureManager.Repo.Get(a => a.Name == metadataStructureName.ToString()).FirstOrDefault();
+
+                    //get entitytype
+                    var entityType = entityManager.FindByName("Publication");
+
+                    if (md != null)
+                    {
+                        CreateTaskmanager taskManager = new CreateTaskmanager();
+                        taskManager.AddToBus(CreateTaskmanager.METADATASTRUCTURE_ID, md.Id);
+
+                        taskManager.AddToBus(CreateTaskmanager.SAVE_WITH_ERRORS, false);
+                        taskManager.AddToBus(CreateTaskmanager.NO_IMPORT_ACTION, true);
+
+                        // get existing researchPlan
+                        string researchPlanName = "Research plan";
+                        ResearchPlan researchPlan = researchPlanManager.Repo.Get(r => researchPlanName.Equals(r.Title)).FirstOrDefault();
+                        taskManager.AddToBus(CreateTaskmanager.RESEARCHPLAN_ID, researchPlan.Id);
+
+                        //create unstructured datastructure
+                        DataStructure dataStructure = CreateDataStructure("Publication-File") as UnStructuredDataStructure;
+                        taskManager.AddToBus(CreateTaskmanager.DATASTRUCTURE_ID, dataStructure.Id);
+
+                        HttpContext.Session["CreateDatasetTaskmanager"] = taskManager;
+                        setAdditionalFunctions();
+
+                        var view = this.Render("DCM", "Form", "StartMetadataEditor", new RouteValueDictionary()
+                        { });
+
+                        return Content(view.ToHtmlString(), "text/html");
+                    }
+
+                    else
+                    {
+                        return Content("Metadata structure not exsits");
+                    }
                 }
             }
-        
         }
+
+        public ActionResult LoadMetaDataForm(SetupModel model)
+        {
+            if (model.SelectedMetadataStructureId == -1)
+                ModelState.AddModelError("SelectedMetadataStructureId", "Please select a metadata structure.");
+
+            if (ModelState.IsValid)
+            {
+                using (var metadataStructureManager = new MetadataStructureManager())
+                using (var entityManager = new EntityManager())
+                using (var researchPlanManager = new ResearchPlanManager())
+                {
+                    var md = metadataStructureManager.Repo.Get(a => a.Id == model.SelectedMetadataStructureId).FirstOrDefault();
+
+                    //get entitytype
+                    var entityType = entityManager.FindByName("Publication");
+
+                    if (md != null)
+                    {
+                        CreateTaskmanager taskManager = new CreateTaskmanager();
+                        taskManager.AddToBus(CreateTaskmanager.METADATASTRUCTURE_ID, md.Id);
+
+                        taskManager.AddToBus(CreateTaskmanager.SAVE_WITH_ERRORS, false);
+                        taskManager.AddToBus(CreateTaskmanager.NO_IMPORT_ACTION, true);
+
+                        // get existing researchPlan
+                        string researchPlanName = "Research plan";
+                        ResearchPlan researchPlan = researchPlanManager.Repo.Get(r => researchPlanName.Equals(r.Title)).FirstOrDefault();
+                        taskManager.AddToBus(CreateTaskmanager.RESEARCHPLAN_ID, researchPlan.Id);
+
+                        //create unstructured datastructure
+                        DataStructure dataStructure = CreateDataStructure("Publication-File") as UnStructuredDataStructure;
+                        taskManager.AddToBus(CreateTaskmanager.DATASTRUCTURE_ID, dataStructure.Id);
+
+                        HttpContext.Session["CreateDatasetTaskmanager"] = taskManager;
+                        setAdditionalFunctions();
+
+                        var view = this.Render("DCM", "Form", "StartMetadataEditor", new RouteValueDictionary() { });
+
+                        return Content(view.ToHtmlString(), "text/html");
+                    }
+
+                    else
+                        return Content("Metadata structure not exsits");
+                }
+
+            }
+            else
+                return Content("Metadata structure not exsits");
+
+        }
+
         public JsonResult Submit(bool valid)
         {
             // create and submit Dataset
@@ -169,6 +249,33 @@ namespace BExIS.Modules.PUB.UI.Controllers
                 }
             }
         }
+
+        #region Helper
+
+        private bool HasEntityTypeName(MetadataStructure metadataStructure, string entityName)
+        {
+
+            // get MetadataStructure
+            if (metadataStructure != null && metadataStructure.Extra != null)
+            {
+                XDocument xDoc = XmlUtility.ToXDocument((XmlDocument)metadataStructure.Extra);
+                IEnumerable<XElement> tmp = XmlUtility.GetXElementByNodeName(nodeNames.entity.ToString(), xDoc);
+                if (tmp.Any())
+                {
+                    foreach (var entity in tmp)
+                    {
+                        string tmpEntityClassPath = "";
+                        if (entity.HasAttributes && entity.Attribute("name") != null)
+                            tmpEntityClassPath = entity.Attribute("name").Value.ToLower();
+
+                        if (tmpEntityClassPath.Equals(entityName.ToLower())) return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        #endregion
 
     }
 }
---
+++ BEXIS.Modules.PUB.UI/Models/SetupModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.PUB.UI.Models
+{
+    public class SetupModel
+    {
+        [Display(Name = "Metadata Structure")]
+        [Required(ErrorMessage = "Please select a metadata structure.")]
+        public long SelectedMetadataStructureId { get; set; }
+
+        public List<ViewSelectItem> MetadataStructureList { get; set; }
+
+        public SetupModel()
+        {
+            SelectedMetadataStructureId = -1;
+            MetadataStructureList = new List<ViewSelectItem>();
+        }
+
+
+    }
+
+    public class ViewSelectItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+
+        public ViewSelectItem(long id, string titel)
+        {
+            Id = id;
+            Title = titel;
+        }
+
+    }
+}
